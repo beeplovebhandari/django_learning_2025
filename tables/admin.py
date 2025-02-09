73562069ff03c6f50b2549d8b0f6d3eebfa18cff
beeplovebhandari <bbhandari0304@gmail.com>
2025-02-09 21:07:58 +0545

    One to One Model Relation

--- tables/admin.py
+++ tables/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Student, ClassRoom
+from .models import Student, ClassRoom, StudentProfile
 
 admin.site.register(Student)
 admin.site.register(ClassRoom)
+admin.site.register(StudentProfile)
