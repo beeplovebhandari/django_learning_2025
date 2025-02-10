a08e041b2cb371c7f1b57f01a300479012ced672
beeplovebhandari <bbhandari0304@gmail.com>
2025-02-10 21:36:27 +0545

    Classroom Model Created

--- tables/models.py
+++ tables/models.py
@@ -11,6 +11,7 @@ class Student(models.Model):
     age = models.IntegerField()
     address = models.CharField(max_length=20)
     bio = models.TextField()
+    classroom = models.ForeignKey(ClassRoom, on_delete=models.CASCADE, related_name="classroom_students", null=True, blank=True)
 
     
     def __str__(self):
