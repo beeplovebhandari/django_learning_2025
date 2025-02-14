2f0a4523279af44ca501013333c10dd0042a2649
beeplovebhandari <bbhandari0304@gmail.com>
2025-02-14 22:10:36 +0545

    Many to Many relations

--- tables/models.py
+++ tables/models.py
@@ -24,10 +24,22 @@ class StudentProfile(models.Model):
     roll_no = models.IntegerField()
 
 
+class Publication(models.Model):
+    title = models.CharField(max_length=20)
+
+
+    def __str__(self):
+        return self.title
+
+
+
+class Article(models.Model):
+    headline = models.CharField(max_length=20)
+    publications = models.ManyToManyField(Publication)
+
     
-
-
-
+    def __str__(self):
+        return self.headline
 
 
 
