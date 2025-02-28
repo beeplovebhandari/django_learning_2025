66f05fbd8e3965b0e0d98fada88b62614c48cae5
beeplovebhandari <bbhandari0304@gmail.com>
2025-02-28 21:40:11 +0545

    Crud Student

--- crud/views.py
+++ crud/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import ClassRoom
+from .models import ClassRoom, Student, StudentProfile
 
 
 def crud_classroom(request):
@@ -27,3 +27,28 @@ def classroom_update(request, id):
         ClassRoom.objects.filter(id=id).update(name=name)
         return redirect("crud_classroom")
     return render(request, template_name="crud/classroom_update.html", context={"classroom": classroom})
+
+
+def crud_student(request):
+    return render(request, template_name="crud/student.html", context={"students": Student.objects.all(), "title": "Student"})
+
+
+
+def add_student(request):
+    if request.method=="POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        age = request.POST.get("age")
+        address = request.POST.get("address")
+        contact = request.POST.get("contact")
+        pp = request.FILES.get('pp')
+        classroom_name = request.POST.get("classroom_name")
+        classroom = ClassRoom.objects.get(name=classroom_name)
+        student = Student.objects.create(name=name, email=email, age=age, classroom=classroom )
+        sp = StudentProfile.objects.create(address=address, contact=contact, student=student)
+        if pp: 
+            sp.profile_picture = pp
+            sp.save()
+        return redirect("crud_student")
+    classrooms = ClassRoom.objects.all()
+    return render(request, template_name="crud/add_student.html", context={"title": "Add Student", "classrooms": classrooms})
